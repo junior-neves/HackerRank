1c83b528c9dddefd8a8fdfe6b1167fac56ef05ed
junior-neves <junior.neves@outlook.com>
2018-08-27 21:55:19 -0300

    Update 'Plus Minus'

--- problem-solving/algorithms-warmup/plus-minus.php
+++ problem-solving/algorithms-warmup/plus-minus.php
@@ -1,7 +1,15 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Junior
- * Date: 27/08/2018
- * Time: 21:47
- */
+
+function plusMinus($arr) {
+    $totalArray = count($arr);
+    $totalP = $totalN = $totalZ = 0;
+    foreach ($arr as $number) {
+        if ($number == 0) { $totalZ++; }
+        else if ($number > 0) { $totalP++; }
+        else { $totalN++; }
+    }
+
+    print($totalP/$totalArray."\n");
+    print($totalN/$totalArray."\n");
+    print($totalZ/$totalArray);
+}
